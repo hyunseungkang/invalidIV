73fc0854e66a735074abf349798551d264a0a1ad
Hyunseung Kang <hskang@stanford.edu>
2016-10-28 18:14:05 -0700

    Modified TSHT and Example Code to handle Javanmard and Montanari Code

--- TSHT.R
+++ TSHT.R
@@ -19,7 +19,7 @@
 ###        intercept, should the intercept term be included? (TRUE)
 ###        alpha, a numeric value indicating the significance level for 
 ###               confidence interval
-###        estimator, choose either Javanmard & Montanari or HDI package
+###        tuning, a constant value greater than 2
 ### Output: a list of (a) beta (scalar numeric value:
 ###                             the estimate of the treatment effect)
 ###                   (b) se (scalar numeric value:
@@ -29,9 +29,9 @@
 ###                           with the lower and upper endpoints)
 ###                   (d) V (numeric vector denoting the set of valid and relevant IVs)
 ###                   (e) S (numeric vector denoting the set of relevant IVs)
-TSHT.hdim <- function(Y,D,Z,X,intercept=TRUE,alpha=0.05,estimator=c("hdi","JM")){
+TSHT.hdim <- function(Y,D,Z,X,intercept=TRUE,alpha=0.05,tuning=2.01){
   # Constants
-  n = length(Y)
+  n = length(Y); pz=ncol(Z)
   
   # Setting Up W Matrix
   if(missing(X)) {
@@ -39,60 +39,30 @@ TSHT.hdim <- function(Y,D,Z,X,intercept=TRUE,alpha=0.05,estimator=c("hdi","JM"))
   } else{
     W = cbind(Z,X)
   }
-  #if(intercept) {
-  #  W = cbind(W,1)
-  #}
+  p = ncol(W)
   
-  # First Part (ITT Estimation)#
-  if(estimator[1] == "hdi") {
-    model_Y = lasso.proj(W,Y)
-    ITT_Y = model_Y$bhat[1:ncol(Z)]
-    residY = (Y - W %*% model_Y$betahat)
-    SigmaSqY = sum(residY^2)/n
-    
-    model_D = lasso.proj(W,D)
-    ITT_D = model_D$bhat[1:ncol(Z)]
-    residD = (D - W %*% model_D$betahat)
-    SigmaSqD = sum(residD^2)/n
-    
-    SigmaYD = sum(residD * residY)/n
-    
-    # ZIJIAN: How do you estimate this from the hdi comment
-    # How do you estimate covariance matrix of $W$?
-    covW = t(W) %*% W / n
-  }
-  if(estimator[1] == "JM") {
-    pz=ncol(Z)
-    model_Y <- SSLasso(W,Y,intercept,verbose=FALSE)
-    model_D = SSLasso(W,D,intercept,verbose=FALSE) 
-    # ZIJIAN: How do you estimate this from Javanmard's code? Should we extract the $M$ from InverseLinfinity?
-    #covW = t(W) %*% W / n
-    ITT_Y = model_Y$unb.coef[1:(pz)]
-    ITT_D = model_D$unb.coef[1:(pz)]
-    if(intercept==TRUE){
-    covWinv =(model_D$proj %*% t(model_D$proj)/n)[2:(pz+1),2:(pz+1)]
-    W.whole<-cbind(rep(1,n),W)
-    covW<- t(W.whole)%*%(W.whole)/n
-    resid_Y<-Y-W.whole%*%model_Y$coef
-    resid_D<-D-W.whole%*%model_D$coef
-    Resid<-cbind(resid_Y,resid_D)
-    Theta.hat<-t(Resid)%*%Resid/n
-    SigmaSqY=Theta.hat[1,1]
-    SigmaSqD=Theta.hat[2,2]
-    SigmaYD =Theta.hat[1,2]
+  # Fit Reduced-Form Model for Y and D
+  model_Y <- SSLasso(X=W,y=Y,intercept=intercept,verbose=FALSE)
+  model_D = SSLasso(X=W,y=D,intercept=intercept,verbose=FALSE) 
+  ITT_Y = model_Y$unb.coef[1:(pz)]
+  ITT_D = model_D$unb.coef[1:(pz)]
+  resid_Y = model_Y$resid.lasso; resid_D = model_D$resid.lasso
+  SigmaSqY=sum(resid_Y^2)/n
+  SigmaSqD=sum(resid_D^2)/n
+  SigmaYD =sum(resid_Y * resid_D)/n
+  
+  WUMat = model_D$WUMat
+  if(intercept) {
+    W = cbind(W,1)
+    covW = t(W) %*% W/n
   } else{
-    covWinv =(model_D$proj %*% t(model_D$proj)/n)[1:(pz),1:(pz)]
-    W.whole<-W
-    covW<- t(W.whole)%*%(W.whole)/n
-    resid_Y<-Y-W.whole%*%model_Y$coef
-    resid_D<-D-W.whole%*%model_D$coef
-    Resid<-cbind(resid_Y,resid_D)
-    Theta.hat<-t(Resid)%*%Resid/n
-    SigmaSqY=Theta.hat[1,1]
-    SigmaSqD=Theta.hat[2,2]
-    SigmaYD =Theta.hat[1,2]
+    covW = t(W) %*% W/n
   }
-  return(TSHT.summ(ITT_Y = ITT_Y,ITT_D = ITT_D,SigmaSqD = SigmaSqD,SigmaSqY = SigmaSqY,SigmaYD=SigmaYD,covW=covW,covWinv=covWinv,n=n,alpha=alpha))
+  
+  return(TSHT.helper(ITT_Y = ITT_Y,ITT_D = ITT_D,
+                     SigmaSqD = SigmaSqD,SigmaSqY = SigmaSqY,SigmaYD=SigmaYD,
+                     covW = covW,WUMat = WUMat,
+                     weightedBeta=FALSE,alpha=alpha,tuning=tuning))
 }
 
 ### TSHT.ldim
@@ -107,6 +77,7 @@ TSHT.hdim <- function(Y,D,Z,X,intercept=TRUE,alpha=0.05,estimator=c("hdi","JM"))
 ###        intercept, should the intercept term be included? (TRUE)
 ###        alpha, a numeric value indicating the significance level for 
 ###               confidence interval
+###        tuning, a constant value greater than 2
 ### Output: a list of (a) beta (scalar numeric value:
 ###                             the estimate of the treatment effect)
 ###                   (b) se (scalar numeric value:
@@ -116,9 +87,9 @@ TSHT.hdim <- function(Y,D,Z,X,intercept=TRUE,alpha=0.05,estimator=c("hdi","JM"))
 ###                           with the lower and upper endpoints)
 ###                   (d) V (numeric vector denoting the set of valid and relevant IVs)
 ###                   (e) S (numeric vector denoting the set of relevant IVs)
-TSHT.ldim <- function(Y,D,Z,X,intercept=FALSE,alpha=0.05) {
+TSHT.ldim <- function(Y,D,Z,X,intercept=TRUE,alpha=0.05,tuning=2.01) {
   # Constants
-  n = length(Y)
+  n = length(Y); pz=ncol(Z)
   
   # Setting Up W Matrix
   if(missing(X)) {
@@ -126,25 +97,32 @@ TSHT.ldim <- function(Y,D,Z,X,intercept=FALSE,alpha=0.05) {
   } else{
     W = cbind(Z,X)
   }
+  p = ncol(W)
+  
+  # Include intercept
   if(intercept) {
     W = cbind(W,1)
-  }
+  } 
+  
+  # Compute covariance of W and W %*% U
+  covW = t(W) %*% W /n
+  WUMat = W %*% (solve(covW))[,1:p]
   
   # First Part (OLS Estimation)
   qrW = qr(W)
-  ITT_Y = qr.coef(qrW,Y)[1:ncol(Z)]
-  ITT_D = qr.coef(qrW,D)[1:ncol(Z)]
+  ITT_Y = qr.coef(qrW,Y)[1:pz]
+  ITT_D = qr.coef(qrW,D)[1:pz]
   SigmaSqY = sum(qr.resid(qrW,Y)^2)/n
   SigmaSqD = sum(qr.resid(qrW,D)^2)/n
   SigmaYD = sum(qr.resid(qrW,Y) * qr.resid(qrW,D)) / n
-  covW = t(W) %*% W / n
-  covWinv = solve(covW)
-  pz=ncol(Z)
+
   # Second Part (Invalid IV Estimation)
-  return(TSHT.summ(ITT_Y = ITT_Y,ITT_D = ITT_D,SigmaSqD = SigmaSqD,SigmaSqY = SigmaSqY,SigmaYD=SigmaYD,covW=covW,covWinv = covWinv,n=n,alpha=alpha))
+  TSHT.helper(ITT_Y = ITT_Y,ITT_D = ITT_D,
+              SigmaSqD = SigmaSqD,SigmaSqY = SigmaSqY,SigmaYD=SigmaYD,
+              covW = covW,WUMat = WUMat,weightedBeta = TRUE,alpha=alpha,tuning=tuning)
 }
 
-### TSHT.summ
+### TSHT.helper
 ### Function: Point estimate and CI for treatment effect with invalid IVs ###
 ###           using summary statistics 
 ### Input: ITT_Y, a p_z by 1 vector of estimated values of each IV's 
@@ -157,16 +135,8 @@ TSHT.ldim <- function(Y,D,Z,X,intercept=FALSE,alpha=0.05) {
 ###               variance of the outcome
 ###        SigmaYD, a positive scalar value of the estimated 
 ###                 covariance between the outcome and treatment
-###        covW, the estimated p by p covariance matrix of the 
-###              concatenated matrix consisting of p_z IVs and 
-###              p_x covariates (i.e. p by p matrix where
-###              p = p_z + px)
-###        covWinv, the estimated p by p inverse covariance 
-###                 of the concatenated matrix consisting of 
-###                 p_z IVs and p_x covariats (i.e. p by p
-###                 matrix where p = p_z + p_x).
-###                 Either covW or covWinv must be provided.
-###        n, sample size used in the estimates above
+###        covW, estimmate of E(W^T W), used for weighting beta 
+###        WUMat, the matrix W %*% U in the thresholding procedure.
 ###        weightedBeta, should we weigh the beta? Only applies to case where n > p and covW is provided
 ###        alpha, a numeric value indicating the significance level for 
 ###               confidence interval
@@ -180,64 +150,68 @@ TSHT.ldim <- function(Y,D,Z,X,intercept=FALSE,alpha=0.05) {
 ###                           with the lower and upper endpoints)
 ###                   (d) V (numeric vector denoting the set of valid and relevant IVs)
 ###                   (e) S (numeric vector denoting the set of relevant IVs)
-TSHT.summ <- function(ITT_Y,ITT_D,SigmaSqD,SigmaSqY,SigmaYD,covW,covWinv,n,weightedBeta = FALSE,alpha=0.05,tuning=2.01) {
+TSHT.helper <- function(ITT_Y,ITT_D,
+                        SigmaSqD,SigmaSqY,SigmaYD,
+                        covW,WUMat,
+                        weightedBeta = FALSE,alpha=0.05,tuning=2.01) {
+  n = nrow(WUMat)
   pz = length(ITT_Y)
-  # ZIJIAN: Why do you distinguish the intercept here? Can't we residualize the intercept by de-meaning the estimates?
-  #if(missing(covWinv)&intercept==TRUE) {
-  #  covWinv = solve(covW)[2:(pz+1),2:(pz+1)]
-  #}
-  #if(missing(covWinv)&intercept==FALSE) {
-  #  covWinv = solve(covW)[1:(pz),1:(pz)]
-  #}
   
   # First Stage
-  Stilde = (1:length(ITT_D))[(abs(ITT_D) >= sqrt(SigmaSqD) * sqrt(diag(covWinv)) * sqrt(tuning*log(max(pz,n))/n))]
+  Stilde = (1:length(ITT_D))[abs(ITT_D) >= (sqrt(SigmaSqD) * sqrt(colSums(WUMat^2)/n) * sqrt(tuning*log(max(pz,n))/n))]
+  suppgamma = rep(FALSE,length(ITT_D))
+  suppgamma[Stilde] = TRUE
   
   # Second Stage
-  invalidIV.candidate = matrix(FALSE,length(ITT_Y),length(Stilde))
-  pi.candidate = matrix(0,length(ITT_Y),length(Stilde))
+  # pi.candidate is the estimated value of pi across different candidates
+  pi.candidate = matrix(0,length(ITT_Y),length(Stilde)) 
   for(i in 1:length(Stilde)) {
     j = Stilde[i]
     betaj = ITT_Y[j] / ITT_D[j]
-    pi.candidate.noHT = ITT_Y - ITT_D * betaj
-    sigmaSq.candidate = SigmaSqY + betaj^2 * SigmaSqD - 2*betaj*SigmaYD #Is it possible to use reduced-form sigma directly?
-	pi.candidate.HT.index = (1:length(ITT_Y))[(abs(pi.candidate.noHT) >= tuning * sqrt(sigmaSq.candidate) * 
-                                                 sqrt(diag(covWinv)- 2 *ITT_D / ITT_D[j] * covWinv[j,] + ITT_D^2 / ITT_D[j]^2 * covWinv[j,j]) * 
-                                                 sqrt(log(max(pz,n))/n))]
-    invalidIV.candidate[intersect(Stilde,pi.candidate.HT.index),i] = TRUE
-    pi.candidate[,i] = 0; 
-    pi.candidate[invalidIV.candidate[,i],i] = pi.candidate.noHT[invalidIV.candidate[,i]]
+    pi.candidate.noHT = ITT_Y - ITT_D * betaj #HT stands for hard thresholding
+    sigmaSq.candidate = SigmaSqY + betaj^2 * SigmaSqD - 2*betaj*SigmaYD
+	  pi.candidate.HT.index = (1:length(ITT_Y))[suppgamma & #this line here essentially does k \in Stilde in our paper
+	                                            (abs(pi.candidate.noHT) >= tuning * sqrt(sigmaSq.candidate) * 
+	                                              sqrt(colSums( (WUMat - outer(WUMat[,j]/ITT_D[j], ITT_D))^2)/n) *
+                                                sqrt(log(max(pz,n))/n))]
+    pi.candidate[pi.candidate.HT.index,i] = pi.candidate.noHT[pi.candidate.HT.index]
   }
   
   # L0 Penalization
-  invalidIV.candidate.l0 = colSums(invalidIV.candidate)
-  jstar = which(min(invalidIV.candidate.l0) == invalidIV.candidate.l0)
+  pi.candidate.l0 = colSums(abs(pi.candidate) > 0) #The inner operation doesn't turn abs(pi.candidate) > 0 into a vector.
+  jstar = which(min(pi.candidate.l0) == pi.candidate.l0)
   if(length(jstar) > 1) {
     # L1 Penalization if non-unique form exists
-    invalidIV.candidate.l1 = colSums(abs(pi.candidate[,jstar]))
-    jstar = jstar[which(min(invalidIV.candidate.l1) == invalidIV.candidate.l1)]
-    if(length(jstar) > 1) jstar = jstar[1]
+    pi.candidate.l1 = colSums(abs(pi.candidate[,jstar]))
+    minL1amongNonUniqueL0 = which(min(pi.candidate.l1) == pi.candidate.l1) 
+    jstar = jstar[minL1amongNonUniqueL0]
+    if(length(jstar) > 1) jstar = jstar[1] #If it's still non-unique, we just choose the first one.
   }
-
+  
+  # Estimated support of pi 
+  supppi = which(abs(pi.candidate[,jstar]) > 0) 
+  
   # Vtilde and Aweight
-  Vtilde = (Stilde)[!(Stilde %in% which(invalidIV.candidate[,jstar] == TRUE))]
-  if(ncol(covW) < (n/10) & weightedBeta & !missing(covW)) {
+  Vtilde = setdiff(Stilde,supppi) #this is a setminus operation with Stilde \setminus supp(Pi)
+  if((ncol(covW) < (n/10) || weightedBeta) && !missing(covW)) {
     Aweight = covW[Vtilde,Vtilde] - 
               covW[Vtilde,-Vtilde] %*% (solve(covW))[-Vtilde,-Vtilde] %*% covW[-Vtilde,Vtilde]
+    commonDenom = (t(ITT_D[Vtilde]) %*% Aweight %*% ITT_D[Vtilde])
     
     ### betaE, standard error, and conf int.
-    commonDenom = (t(ITT_D[Vtilde]) %*% Aweight %*% ITT_D[Vtilde])
     betaE = (t(ITT_D[Vtilde]) %*% Aweight %*% ITT_Y[Vtilde]) / commonDenom
-    VE = (SigmaSqY + betaE^2 * SigmaSqD - 2*betaE*SigmaYD)/ commonDenom
-    confInt = c(betaE - qnorm(1-alpha/2) * sqrt(VE/n),betaE + qnorm(1-alpha/2) * sqrt(VE/n))
+    sigmaSq = SigmaSqY + betaE^2 * SigmaSqD - 2*betaE*SigmaYD
+    V = sigmaSq / commonDenom
+    confInt = c(betaE - qnorm(1-alpha/2) * sqrt(V/n),betaE + qnorm(1-alpha/2) * sqrt(V/n))
     return(list(beta=betaE,se = sqrt(VE/n),ci = confInt,V=Vtilde,S = Stilde))  
   } else {
-    Aweight = diag(1,length(Vtilde),length(Vtilde))
-    commonDenom = (t(ITT_D[Vtilde]) %*% Aweight %*% ITT_D[Vtilde])
+    commonDenom = sum(ITT_D[Vtilde]^2)
     
-    ### General beta estimator betaG, standard error and 
-    betaG = (t(ITT_D[Vtilde]) %*% Aweight %*% ITT_Y[Vtilde]) / commonDenom
-    VG = (SigmaSqY + betaG^2 * SigmaSqD - 2*betaG*SigmaYD)*(t(ITT_D[Vtilde]) %*% covWinv[Vtilde,Vtilde]%*%ITT_D[Vtilde])/ (commonDenom)^2
+    ### General beta estimator betaG
+    betaG = sum(ITT_D[Vtilde] * ITT_Y[Vtilde]) / commonDenom
+    sigmaSq = SigmaSqY + betaG^2 * SigmaSqD - 2*betaG*SigmaYD
+    VG = sigmaSq *
+         sum((WUMat[,Vtilde] %*% ITT_D[Vtilde] / sqrt(n))^2) / (commonDenom)^2
     confInt = c(betaG - qnorm(1-alpha/2) * sqrt(VG/n),betaG + qnorm(1-alpha/2) * sqrt(VG/n))
     return(list(beta=betaG,se = sqrt(VG/n),ci = confInt,V=Vtilde,S = Stilde))
   }
--- example.R
+++ example.R
@@ -1,12 +1,12 @@
 ### Obtain the two-stage hard thresholding (TSHT) code from Github ###  
 source("https://raw.githubusercontent.com/hyunseungkang/invalidIV/master/TSHT.R")
+source("https://raw.githubusercontent.com/hyunseungkang/invalidIV/master/JMCode.R")
 
 ### R Packages to Load ###
-# The AER and MASS packages are only needed to run the working example.
+# The AER package is only needed to run the working example.
 library(AER)
-library(MASS)
 
-### Working Example ###
+### Working Low Dimensional Example ###
 ### n = 500, pz = 10 IVs (s = 3 invalid)
 # Y: n by 1 vector of outcomes (must be continuous)
 # D: n by 1 vector of treatments (continuous or discrete)
@@ -21,8 +21,8 @@ epsilonSigma = matrix(c(1,0.8,0.8,1),2,2)
 Z = matrix(rnorm(n*L),n,L)
 
 epsilon = mvrnorm(n,rep(0,2),epsilonSigma)
-D = Z %*% gamma + epsilon[,1]
-Y = Z %*% alpha + D * beta + epsilon[,2]
+D = 0.5 + Z %*% gamma + epsilon[,1]
+Y = -0.5 + Z %*% alpha + D * beta + epsilon[,2]
 
 ### Oracle Two-Stage Least Squares Estimator ###
 # This oracle knows exactly which IVs are invalid
@@ -30,8 +30,8 @@ Y = Z %*% alpha + D * beta + epsilon[,2]
 # associated with D close to 1 and the 95% confidence
 # interval covering 1
 
-summary(ivreg(Y ~ D + Z[,1:s] - 1 | Z - 1))
-confint(ivreg(Y ~ D + Z[,1:s] - 1 | Z - 1))[1,]
+summary(ivreg(Y ~ D + Z[,1:s]  | Z ))
+confint(ivreg(Y ~ D + Z[,1:s]  | Z ))[2,]
 
 
 ### Our TSHT Estimator ###
@@ -46,3 +46,51 @@ confint(ivreg(Y ~ D + Z[,1:s] - 1 | Z - 1))[1,]
 # V: estimated set of valid IVs
 # S: estimated set of relevant IVs
 TSHT.ldim(Y,D,Z)
+
+
+
+### Working High Dimensional Example ###
+### You need the following packages to run the high dimensional code 
+library(Matrix)
+library(glmnet)
+library(flare)
+
+### n = 100, pz = 100 IVs (s = 3 invalid, 10 relevant)
+# Y: n by 1 vector of outcomes (must be continuous)
+# D: n by 1 vector of treatments (continuous or discrete)
+# Z: n by pz vector of instruments (continuous or discrete)
+# beta:   true treatment effect (set at 1)
+
+# Create data #
+library(MASS)
+n = 100; L = 100; s = 3; nRelevant = 10
+alpha = c(rep(3,s),rep(0,L-s)); beta = 1; gamma = c(rep(1,nRelevant),rep(0,L-nRelevant))
+epsilonSigma = matrix(c(1,0.8,0.8,1),2,2)
+Z = matrix(rnorm(n*L),n,L)
+
+epsilon = mvrnorm(n,rep(0,2),epsilonSigma)
+D =  0.5 + Z %*% gamma + epsilon[,1]
+Y = -0.5 + Z %*% alpha + D * beta + epsilon[,2]
+
+### Oracle Two-Stage Least Squares Estimator ###
+# This oracle knows exactly which IVs are invalid
+# and should perform very well,   with the point estimate
+# associated with D close to 1 and the 95% confidence
+# interval covering 1
+
+summary(ivreg(Y ~ D + Z[,1:s]  | Z[,1:nRelevant] ))
+confint(ivreg(Y ~ D + Z[,1:s]  | Z[,1:nRelevant] ))[2,]
+
+
+### Our TSHT Estimator ###
+# Our estimator does not assume which IVs are invalid
+# a priori. It should perform as well as the the oracle
+# above as sample size increases.
+
+# Output is a list that includes
+# beta: point estimate of the treatment effect
+# se: standard error of the beta
+# ci: 1 - alpha confidence interval for beta
+# V: estimated set of valid IVs
+# S: estimated set of relevant IVs
+TSHT.hdim(Y,D,Z)
